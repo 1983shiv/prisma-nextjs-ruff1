14fa939f4baf81b43b8868d777940b6f8758719b
shiv <1983.shiv@gmail.com>
2026-01-30 10:06:34 +0530

    revised next config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // Move 'turbopack' out of 'experimental'
+  turbopack: {
+    resolveAlias: {
+      '@/app/generated/prisma/client': './app/generated/prisma/client',
+    },
+  },
   experimental: {
     serverComponentsExternalPackages: ['@prisma/client'],    
   },
